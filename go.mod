3de83184a134fd3fea2a8dd6e974e8abbcdefcba
polyhistor2050 <chrisspaul2050@gmail.com>
2024-04-24 01:58:33 +0300

    Load to environment variables file on the main file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/polyhistor2050/Go-Auth-API
 
 go 1.22.2
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello, World")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("Port:", portString)
 }
